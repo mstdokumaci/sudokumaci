ea8a21891f7454e157fa489a6a0b3b8760a5a71a
Mustafa Dokumacı <mustafa.dokumaci@digib.com>
2023-05-29 19:13:18 +0200

    solve all sudokus

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,31 +1,36 @@
 #[macro_use]
 extern crate lazy_static;
 
-use rayon::prelude::*;
-use std::env;
-use std::fs;
+// use rayon::prelude::*;
+// use std::env;
+// use std::fs;
 
+mod make_list;
 mod sudoku;
 
+// fn main() {
+//     let filename = match env::args().nth(1) {
+//         Some(filename) => filename,
+//         None => {
+//             println!("Usage: sudoku <filename>");
+//             return;
+//         }
+//     };
+
+//     let sudokus = fs::read_to_string(filename).unwrap();
+
+//     print!(
+//         "{}",
+//         sudokus
+//             .lines()
+//             .collect::<Vec<&str>>()
+//             .par_iter()
+//             .map(|puzzle| format!("{},{}", *puzzle, sudoku::solve(*puzzle)))
+//             .collect::<Vec<String>>()
+//             .join("\n")
+//     );
+// }
+
 fn main() {
-  let filename = match env::args().nth(1) {
-    Some(filename) => filename,
-    None => {
-      println!("Usage: sudoku <filename>");
-      return;
-    }
-  };
-
-  let sudokus = fs::read_to_string(filename).unwrap();
-
-  print!(
-    "{}",
-    sudokus
-      .lines()
-      .collect::<Vec<&str>>()
-      .par_iter()
-      .map(|puzzle| format!("{},{}", *puzzle, sudoku::solve(*puzzle)))
-      .collect::<Vec<String>>()
-      .join("\n")
-  );
+    make_list::make_possible_combinations();
 }
