2500c62d420ac77ccfe7a23bef49484c81ca5c76
Mustafa Dokumacı <mustafa@foraa.com>
2025-02-13 20:06:54 +0100

    less significant improvements

--- zig/main.zig
+++ zig/main.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const Sudoku = @import("sudoku.zig").Sudoku;
 
 pub fn main() !void {
-    const args = try std.process.argsAlloc(std.heap.page_allocator);
-    defer std.process.argsFree(std.heap.page_allocator, args);
+    const args = try std.process.argsAlloc(std.heap.c_allocator);
+    defer std.process.argsFree(std.heap.c_allocator, args);
 
     if (args.len != 2) {
         std.debug.print("Usage: sudokumaci <filename>\n", .{});
@@ -15,8 +15,8 @@ pub fn main() !void {
     const file = try std.fs.cwd().openFile(filename, .{});
     defer file.close();
 
-    const file_contents = try file.readToEndAlloc(std.heap.page_allocator, std.math.maxInt(usize));
-    defer std.heap.page_allocator.free(file_contents);
+    const file_contents = try file.readToEndAlloc(std.heap.c_allocator, std.math.maxInt(usize));
+    defer std.heap.c_allocator.free(file_contents);
 
     const stdout = std.io.getStdOut().writer();
 
--- zig/sudoku.zig
+++ zig/sudoku.zig
@@ -16,7 +16,6 @@ pub const Sudoku = struct {
     is_sudoku: bool = true,
     number_cells: [9]u81 = .{ALL81} ** 9,
     numbers: usize = 0b111111111,
-    band_combinations: [3]u162 = .{ALL162} ** 3,
 
     pub fn solve(self: *Sudoku, cell_values: *const [81]u8) [81]u8 {
         var remove_from_others: [9]u81 = .{0} ** 9;
@@ -31,7 +30,7 @@ pub const Sudoku = struct {
 
         const shortest_number = self.remove_cells(remove_from_others);
         assert(self.is_sudoku);
-        assert(self.find_match(shortest_number));
+        assert(self.find_match(shortest_number, .{ALL162} ** 3));
 
         var solved: [81]u8 = .{48} ** 81;
         for (&self.number_cells, 0..) |*cells, number| {
@@ -92,48 +91,87 @@ pub const Sudoku = struct {
         return if (new_remove) self.remove_cells(new_remove_from_others) else shortest_number;
     }
 
-    fn find_match(self: *Sudoku, number: usize) bool {
+    fn remove_cells_single(self: *Sudoku, remove_from_others: u81) usize {
+        var shortest_length: usize = 81;
+        var shortest_number: usize = 0;
+        var new_remove_from_others: [9]u81 = .{0} ** 9;
+        var new_remove = false;
+
+        var biterate = self.numbers;
+        while (biterate > 0) {
+            const number = @ctz(biterate);
+            const cells = &self.number_cells[number];
+            const removed = cells.* & ~remove_from_others;
+            const ones = @popCount(removed);
+            if (ones < 9) {
+                self.is_sudoku = false;
+                return 0;
+            }
+            if (removed != cells.*) {
+                cells.* = removed;
+                for (GROUPS81) |group_mask| {
+                    const group = cells.* & group_mask;
+                    const group_ones = @popCount(group);
+                    if (group_ones == 0) {
+                        self.is_sudoku = false;
+                        return 0;
+                    } else if (group_ones == 1) {
+                        const set_cells = cells.* & SET81[@ctz(group)];
+                        if (set_cells != cells.*) {
+                            cells.* = set_cells;
+                            new_remove_from_others[number] |= group;
+                            new_remove = true;
+                        }
+                    }
+                }
+            }
+            if (ones < shortest_length) {
+                shortest_length = ones;
+                shortest_number = number;
+            }
+            biterate &= biterate - 1;
+        }
+        return if (new_remove) self.remove_cells(new_remove_from_others) else shortest_number;
+    }
+
+    fn find_match(self: *Sudoku, number: usize, band_combinations: [3]u162) bool {
         self.numbers ^= BIT9[number];
 
         const numbers = self.numbers;
         const number_cells = self.number_cells;
-        const band_combinations = self.band_combinations;
 
         const number_band0: usize = @as(usize, @truncate(number_cells[number] & ALL27));
         const number_band1: usize = @as(usize, @truncate(number_cells[number] >> 27 & ALL27));
         const number_band2: usize = @as(usize, @truncate(number_cells[number] >> 54 & ALL27));
 
+        var new_band_combinations: [3]u162 = undefined;
+
         var biterate0 = band_combinations[0];
         while (biterate0 > 0) {
             const band0_index = @ctz(biterate0);
-            if (number_band0 & POSSIBLES[band0_index] == POSSIBLES[band0_index] and (band_combinations[0] & NUMBER_COMBINATIONS[band0_index] > 0 or self.numbers == 0)) {
+            new_band_combinations[0] = band_combinations[0] & NUMBER_COMBINATIONS[band0_index];
+            if (number_band0 & POSSIBLES[band0_index] == POSSIBLES[band0_index] and (new_band_combinations[0] != 0 or self.numbers == 0)) {
                 var biterate1 = band_combinations[1] & BAND_COMBINATIONS[band0_index];
                 while (biterate1 > 0) {
                     const band1_index = @ctz(biterate1);
-                    if (number_band1 & POSSIBLES[band1_index] == POSSIBLES[band1_index] and (band_combinations[1] & NUMBER_COMBINATIONS[band1_index] > 0 or self.numbers == 0)) {
+                    new_band_combinations[1] = band_combinations[1] & NUMBER_COMBINATIONS[band1_index];
+                    if (number_band1 & POSSIBLES[band1_index] == POSSIBLES[band1_index] and (new_band_combinations[1] != 0 or self.numbers == 0)) {
                         var biterate2 = band_combinations[2] & BAND_COMBINATIONS[band0_index] & BAND_COMBINATIONS[band1_index];
                         while (biterate2 > 0) {
                             const band2_index = @ctz(biterate2);
-                            if (number_band2 & POSSIBLES[band2_index] == POSSIBLES[band2_index] and (band_combinations[2] & NUMBER_COMBINATIONS[band2_index] > 0 or self.numbers == 0)) {
+                            new_band_combinations[2] = band_combinations[2] & NUMBER_COMBINATIONS[band2_index];
+                            if (number_band2 & POSSIBLES[band2_index] == POSSIBLES[band2_index] and (new_band_combinations[2] != 0 or self.numbers == 0)) {
                                 self.number_cells[number] = @as(u81, POSSIBLES[band0_index]) | @as(u81, POSSIBLES[band1_index]) << 27 | @as(u81, POSSIBLES[band2_index]) << 54;
                                 if (self.numbers == 0) {
                                     return true;
                                 }
-                                var new_remove_from_others: [9]u81 = .{0} ** 9;
-                                new_remove_from_others[number] = self.number_cells[number];
-                                const shortest_number = self.remove_cells(new_remove_from_others);
-                                self.band_combinations = .{
-                                    band_combinations[0] & NUMBER_COMBINATIONS[band0_index],
-                                    band_combinations[1] & NUMBER_COMBINATIONS[band1_index],
-                                    band_combinations[2] & NUMBER_COMBINATIONS[band2_index],
-                                };
-                                if (self.is_sudoku and self.find_match(shortest_number)) {
+                                const shortest_number = self.remove_cells_single(self.number_cells[number]);
+                                if (self.is_sudoku and self.find_match(shortest_number, new_band_combinations)) {
                                     return true;
                                 } else {
                                     self.is_sudoku = true;
                                     self.numbers = numbers;
                                     self.number_cells = number_cells;
-                                    self.band_combinations = band_combinations;
                                 }
                             }
                             biterate2 &= biterate2 - 1;
