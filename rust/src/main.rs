30b90ff2b37cf79b773246575b0e05deee6ee958
Mustafa Dokumacı <mustafa@foraa.com>
2025-06-05 16:09:09 +0200

    revert rust to main

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,42 +1,31 @@
+#[macro_use]
+extern crate lazy_static;
+
 use rayon::prelude::*;
 use std::env;
 use std::fs;
 
-mod bitset;
-// mod make_list;
 mod sudoku;
 
 fn main() {
-    let filename = match env::args().nth(1) {
-        Some(filename) => filename,
-        None => {
-            println!("Usage: sudoku <filename>");
-            return;
-        }
-    };
+  let filename = match env::args().nth(1) {
+    Some(filename) => filename,
+    None => {
+      println!("Usage: sudoku <filename>");
+      return;
+    }
+  };
 
-    let sudokus = fs::read_to_string(filename).unwrap();
+  let sudokus = fs::read_to_string(filename).unwrap();
 
-    print!(
-        "{}",
-        sudokus
-            .lines()
-            .collect::<Vec<&str>>()
-            .par_iter()
-            .map(|puzzle| format!("{},{}", *puzzle, sudoku::solve(*puzzle)))
-            .collect::<Vec<String>>()
-            .join("\n")
-    );
+  print!(
+    "{}",
+    sudokus
+      .lines()
+      .collect::<Vec<&str>>()
+      .par_iter()
+      .map(|puzzle| format!("{},{}", *puzzle, sudoku::solve(*puzzle)))
+      .collect::<Vec<String>>()
+      .join("\n")
+  );
 }
-
-// fn main() {
-//     for index in bitset::BitSetTraverse::new((
-//         0b1111111111000101101111111111000101101111111111000000000000000000,
-//         0b0111111111110110110100011111111111111100011111111111111100011111,
-//         0b0000000000000000000000000000001111111111011011011111111111011011,
-//     ))
-//     .into_iter()
-//     {
-//         println!("{}", index)
-//     }
-// }
--- rust/src/sudoku.rs
+++ rust/src/sudoku.rs
@@ -1,30 +1,47 @@
-use crate::bitset;
 pub mod list;
 
+const BIT9: [usize; 9] = list::BIT9;
 const GROUPS: [u128; 27] = list::GROUPS;
 const SET_CELLS: [u128; 81] = list::SET_CELLS;
-const POSSIBLES: [usize; 162] = list::POSSIBLES;
-const POSSIBLE_COMBINATONS: [(usize, usize, usize); 162] = list::POSSIBLE_COMBINATONS;
-const ROW_COMBINATONS: [(usize, usize, usize); 162] = list::ROW_COMBINATONS;
+const POSSIBLE: [[usize; 937]; 54] = list::POSSIBLE;
 const BIT81: [u128; 81] = list::BIT81;
 const ALL81: u128 = list::ALL81;
 
+fn get_bits_list(bits: usize) -> Vec<usize> {
+    BIT9.iter()
+        .enumerate()
+        .filter(|bit| bits & bit.1 != 0)
+        .map(|bit| bit.0)
+        .collect::<Vec<usize>>()
+}
+
+fn get_bits_lists() -> [Vec<usize>; 512] {
+    const INIT: Vec<usize> = vec![];
+    let mut bits_list = [INIT; 512];
+    let mut bits = 0;
+    while bits < 512 {
+        bits_list[bits] = get_bits_list(bits);
+        bits += 1;
+    }
+    bits_list
+}
+
+lazy_static! {
+    pub static ref BITS_LISTS: [Vec<usize>; 512] = get_bits_lists();
+}
+
 struct Board {
     is_sudoku: bool,
+    numbers: usize,
     number_cells: [u128; 9],
-    numbers: [usize; 9],
-    number_rows: [[usize; 3]; 9],
-    number_row_matches: [[usize; 3]; 9],
 }
 
 impl Board {
     fn new(cell_values: [usize; 81]) -> String {
         let mut board = Board {
             is_sudoku: true,
+            numbers: 0b111111111,
             number_cells: [ALL81; 9],
-            numbers: [0; 9],
-            number_rows: [[0; 3]; 9],
-            number_row_matches: [[0; 3]; 9],
         };
 
         let mut remove_from_others = [0; 9];
@@ -38,31 +55,18 @@ impl Board {
             }
         }
 
-        board.remove_from_others(remove_from_others);
-        assert!(board.is_sudoku);
-
-        board.prepare();
-        board.is_sudoku = board.find_match(
-            0,
-            [(
-                0b1111111111111111111111111111111111111111111111111111111111111111,
-                0b1111111111111111111111111111111111111111111111111111111111111111,
-                0b1111111111111111111111111111111111,
-            ); 3],
-        );
+        let shortest = board.remove_from_others(remove_from_others);
+        board.trial_and_error(shortest);
         assert!(board.is_sudoku);
 
         let mut solved: [u32; 81] = [0; 81];
-        for (number, number_row_match) in board.number_row_matches.iter().enumerate() {
-            let mut cells = POSSIBLES[number_row_match[0]] as u128
-                | (POSSIBLES[number_row_match[1]] as u128) << 27
-                | (POSSIBLES[number_row_match[2]] as u128) << 54;
+        for (number, cells) in board.number_cells.iter_mut().enumerate() {
             let mut cell_index = 0;
-            while cells != 0 {
+            while *cells != 0 {
                 let tz = cells.trailing_zeros() as usize;
                 cell_index += tz;
                 solved[cell_index] = (number + 1) as u32;
-                cells >>= tz + 1;
+                *cells >>= tz + 1;
                 cell_index += 1;
             }
         }
@@ -71,103 +75,121 @@ impl Board {
             .map(|n| char::from_digit(*n, 10).unwrap())
             .collect()
     }
-    fn remove_from_others(&mut self, remove_from_others: [u128; 9]) {
+    fn remove_from_others(&mut self, remove_from_others: [u128; 9]) -> (usize, u32) {
+        let mut shortest_length = 81;
+        let mut shortest_number = 0;
         let mut new_remove_from_others = [0; 9];
         let mut new_remove = false;
-        for number in 0..9 {
+        for number in BITS_LISTS.get(self.numbers).unwrap().iter() {
             let mut union = 0;
             for (remove_number, remove) in remove_from_others.iter().enumerate() {
-                if number != remove_number {
+                if *number != remove_number {
                     union |= *remove;
                 }
             }
-            let cells = self.number_cells.get_mut(number).unwrap();
+            let cells = self.number_cells.get_mut(*number).unwrap();
             let removed = *cells & !union;
             if removed != *cells {
                 *cells = removed;
+                let ones = cells.count_ones();
+                if ones < 9 {
+                    self.is_sudoku = false;
+                    return (0, 0);
+                }
                 for group_mask in GROUPS.iter() {
                     let group = *cells & *group_mask;
                     let group_ones = group.count_ones();
                     if group_ones == 0 {
                         self.is_sudoku = false;
-                        return;
+                        return (0, 0);
                     } else if group_ones == 1 {
                         let set_cells = *cells & SET_CELLS[group.trailing_zeros() as usize];
                         if set_cells != *cells {
                             *cells = set_cells;
-                            new_remove_from_others[number] |= group;
+                            new_remove_from_others[*number] |= group;
                             new_remove = true;
                         }
                     }
+                }
+                if ones < shortest_length {
+                    shortest_number = *number;
+                    shortest_length = ones;
+                }
+            } else {
+                let ones = cells.count_ones();
+                if ones < shortest_length {
+                    shortest_number = *number;
+                    shortest_length = ones;
                 }
             }
         }
         if new_remove {
             self.remove_from_others(new_remove_from_others)
+        } else {
+            (shortest_number, shortest_length)
         }
     }
-    fn prepare(&mut self) {
-        let mut number_cells: Vec<(usize, &u128)> = self.number_cells.iter().enumerate().collect();
-        number_cells.sort_unstable_by(|a, b| a.1.count_ones().cmp(&b.1.count_ones()));
-        for (i, (number, _)) in number_cells.iter().enumerate() {
-            self.numbers[i] = *number;
-            self.number_rows[*number] = [
-                (self.number_cells[*number] & 0b111111111111111111111111111) as usize,
-                ((self.number_cells[*number] >> 27) & 0b111111111111111111111111111) as usize,
-                ((self.number_cells[*number] >> 54) & 0b111111111111111111111111111) as usize,
-            ]
-        }
+    fn remove_single_from_others(&mut self, number: usize, cells: u128) -> (usize, u32) {
+        let mut remove_from_others = [0; 9];
+        remove_from_others[number] = cells;
+        self.remove_from_others(remove_from_others)
     }
-    fn find_match(&mut self, number_index: usize, bit_lists: [(usize, usize, usize); 3]) -> bool {
-        let number = self.numbers[number_index];
-        let number_rows = self.number_rows[number];
-        let biterator0 = bitset::BitSetTraverse::new(bit_lists[0]).into_iter();
-        for row0_index in biterator0 {
-            if number_rows[0] & POSSIBLES[row0_index] == POSSIBLES[row0_index] {
-                let biterator1 = bitset::BitSetTraverse::new(bitset::intersect2(
-                    &bit_lists[1],
-                    &ROW_COMBINATONS[row0_index],
-                ))
-                .into_iter();
-                for row1_index in biterator1 {
-                    if number_rows[1] & POSSIBLES[row1_index] == POSSIBLES[row1_index] {
-                        let biterator2 = bitset::BitSetTraverse::new(bitset::intersect3(
-                            &bit_lists[2],
-                            &ROW_COMBINATONS[row0_index],
-                            &ROW_COMBINATONS[row1_index],
-                        ))
-                        .into_iter();
-                        for row2_index in biterator2 {
-                            if (number_rows[2] & POSSIBLES[row2_index] == POSSIBLES[row2_index])
-                                && (number_index == 8
-                                    || self.find_match(
-                                        number_index + 1,
-                                        [
-                                            bitset::intersect2(
-                                                &bit_lists[0],
-                                                &POSSIBLE_COMBINATONS[row0_index],
-                                            ),
-                                            bitset::intersect2(
-                                                &bit_lists[1],
-                                                &POSSIBLE_COMBINATONS[row1_index],
-                                            ),
-                                            bitset::intersect2(
-                                                &bit_lists[2],
-                                                &POSSIBLE_COMBINATONS[row2_index],
-                                            ),
-                                        ],
-                                    ))
-                            {
-                                self.number_row_matches[number] =
-                                    [row0_index, row1_index, row2_index];
-                                return true;
+    fn trial_and_error(&mut self, shortest: (usize, u32)) {
+        if self.is_sudoku == false {
+            return;
+        }
+
+        let (number, length) = shortest;
+
+        self.numbers ^= BIT9[number];
+        let cells = self.number_cells[number];
+
+        if length == 9 {
+            if self.numbers != 0 {
+                let shortest = self.remove_single_from_others(number, cells);
+                self.trial_and_error(shortest);
+            }
+            return;
+        }
+
+        let numbers = self.numbers;
+        let number_cells = self.number_cells.clone();
+
+        let first_group = (cells & 0b111111111111111111) as usize;
+        let second_group = ((cells >> 18) & 0b111111111111111111111111111) as usize;
+        let third_group = ((cells >> 45) & 0b111111111111111111111111111111111111) as usize;
+
+        for sub_list in POSSIBLE.iter() {
+            let first = sub_list[0];
+            if first & first_group == first {
+                for index in (1..925).step_by(13) {
+                    let second = sub_list[index];
+                    if second & second_group == second {
+                        for index in index + 1..index + 13 {
+                            let third = sub_list[index];
+                            if third & third_group == third {
+                                self.number_cells[number] =
+                                    (third as u128) << 45 | (second as u128) << 18 | first as u128;
+                                self.is_sudoku = true;
+                                if self.numbers == 0 {
+                                    return;
+                                }
+                                let shortest = self
+                                    .remove_single_from_others(number, self.number_cells[number]);
+                                self.trial_and_error(shortest);
+                                if self.is_sudoku {
+                                    return;
+                                } else {
+                                    self.numbers = numbers;
+                                    self.number_cells = number_cells.clone();
+                                }
                             }
                         }
                     }
                 }
             }
         }
-        false
+        self.is_sudoku = false;
     }
 }
 
